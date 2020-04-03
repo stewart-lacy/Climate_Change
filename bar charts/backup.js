3545f411466273b905b38292221e9694e8e7a3f0
Daniella DeRose <dcd6395@gmail.com>
2020-04-03 17:07:56 -0500

    interactive plotly

---
+++ bar charts/backup.js
@@ -0,0 +1,19 @@
+d3.json(`data.json`).then((data) => {
+  //console.log(data);
+
+  var trace = {
+    x: ["Qatar", "Trinidad and Tobago", "Kuwait", "United Arab Emirates", "Bahrain"],
+    y: [13.54, 9.32, 6.93, 6.34, 6.28],
+    type: 'bar'
+  };
+  
+  var data1 = [trace]
+
+  var layout = {
+    title: "Countries with Highest CO2 Emissions in 2014",
+    xaxis: { title: "Countries" },
+    yaxis: { title: "Emissions" }};
+  
+  Plotly.newPlot("plot", data1, layout);
+
+});
--- plots/plot.js
+++ plots/plot.js
@@ -1,94 +1,80 @@
-d3.json("data.json").then((data) => {
-  console.log(data);
-  //  Create the Traces
-  var trace1 = {
-    x: data.map(row => row.year_2014),
-    y: data.map(row => row.country_std),
-    text: data.map(row => row.country_std),
-    type: "bar",
-    orientation: "h"
+Plotly.d3.csv('climate_data.csv', function (err, rows) {
+
+  function unpack(rows, key) {
+    return rows.map(function (row) { return row[key]; });
+  }
+
+  var allCountryNames = unpack(rows, 'country'),
+    allYear = unpack(rows, 'year'),
+    allEmissions = unpack(rows, 'per_capita_co2_emissions'),
+    listofCountries = [],
+    currentCountry,
+    currentEmissions = [],
+    currentYear = [];
+
+  for (var i = 0; i < allCountryNames.length; i++) {
+    if (listofCountries.indexOf(allCountryNames[i]) === -1) {
+      listofCountries.push(allCountryNames[i]);
+    }
+  }
+
+  function getCountryData(chosenCountry) {
+    currentEmissions = [];
+    currentYear = [];
+    for (var i = 0; i < allCountryNames.length; i++) {
+      if (allCountryNames[i] === chosenCountry) {
+        currentEmissions.push(allEmissions[i]);
+        currentYear.push(allYear[i]);
+      }
+    }
   };
 
-  //Create the data array for the plot
-  var data = [trace1];
+  // Default Country Data
+  setBubblePlot('Afghanistan');
 
-  // Define the plot layout
-  var layout = {
-    title: "2014 CO2 Emissions per Country",
-    xaxis: { title: "C02 Emissions" },
-    yaxis: { title: "Country" }
+  function setBubblePlot(chosenCountry) {
+    getCountryData(chosenCountry);
+
+    var trace1 = {
+      x: currentYear,
+      y: currentEmissions,
+      mode: 'lines+markers',
+      marker: {
+        size: 12,
+        opacity: 0.5
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: 'Co2 Emissions per Country (1995-2014)',
+      xaxis: { title: "Year" },
+      yaxis: { title: "Co2 Emissions" },
+      height: 400,
+      width: 480
+    };
+
+    Plotly.newPlot('plot', data, layout);
   };
 
-  // Plot the chart to a div tag with id "plot"
-  Plotly.newPlot("plot", data, layout);
-  
-  var year_us  = ["1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"];
+  var innerContainer = document.querySelector('[data-num="0"'),
+    plotEl = innerContainer.querySelector('.plot'),
+    countrySelector = innerContainer.querySelector('.countrydata');
 
-  var emissions = [5.18, 5.24, 5.29, 5.26, 5.3,	5.42,	5.27,	5.26,	5.24,	5.26,	5.25,	5.12,	5.13,	4.93,	4.61,	4.69,	4.56,	4.38,	4.38,	4.43];
+  function assignOptions(textArray, selector) {
+    for (var i = 0; i < textArray.length; i++) {
+      var currentOption = document.createElement('option');
+      currentOption.text = textArray[i];
+      selector.appendChild(currentOption);
+    }
+  }
 
-  var trace2 = {
-    x: year_us,
-    y: emissions,
-    type: "line",
-    orientation: "v"
-  };
+  assignOptions(listofCountries, countrySelector);
 
-  //Create the data array for the plot
-  var data = [trace2];
+  function updateCountry() {
+    setBubblePlot(countrySelector.value);
+  }
 
-  // Define the plot layout
-  var layout = {
-    title: "CO2 Emissions in the United States 1995-2014",
-    xaxis: { title: "Year" },
-    yaxis: { title: "Emissions" }
-  };
-
-  // Plot the chart to a div tag with id "plot"
-  Plotly.newPlot("plot2", data, layout);
-
-  var trace3 = {
-    x: ['1995', '2000', '2005', '2010', '2014'],
-    y: [17.29, 16.03, 16.93, 11.2, 13.54],
-    name: 'Qatar',
-    type: 'bar'
-  };
-  
-  var trace4 = {
-    x: ['1995', '2000', '2005', '2010', '2014'],
-    y: [3.68, 5.03, 7.91, 9.84, 9.32],
-    name: 'Trinidad and Tobago',
-    type: 'bar'
-  };
-
-  var trace5 = {
-    x: ['1995', '2000', '2005', '2010', '2014'],
-    y: [9.18, 7.53, 8.62, 7.99, 6.93],
-    name: 'Kuwait',
-    type: 'bar'
-  };
-
-  var trace6 = {
-    x: ['1995', '2000', '2005', '2010', '2014'],
-    y: [8.2, 10.12, 7.78, 5.26, 6.34],
-    name: 'United Arab Emirates',
-    type: 'bar'
-  };
-
-  var trace7 = {
-    x: ['1995', '2000', '2005', '2010', '2014'],
-    y: [7.23, 7.97, 7.23, 6.33, 6.28],
-    name: 'Bahrain',
-    type: 'bar'
-  };
-  
-  var data = [trace3, trace4, trace5, trace6, trace7];
-  
-  var layout = {
-    barmode: 'stack',
-    title: "Countries with Highest CO2 Emissions in 2014",
-    xaxis: { title: "Year" },
-    yaxis: { title: "Emissions" }};
-  
-  Plotly.newPlot("plot3", data, layout);
-
+  countrySelector.addEventListener('change', updateCountry, false);
 });
